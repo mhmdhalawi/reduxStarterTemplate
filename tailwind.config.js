60f25aa81a6d18ca2f47bbf489bc17031ac3c971
mhmdhalawi <mhmdhalawi@users.noreply.github.com>
2020-12-30 20:43:26 +0200

    added more colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+      white: colors.white,
+      blue: colors.blue,
+    },
   },
   variants: {
     extend: {},
